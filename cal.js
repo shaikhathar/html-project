27578d5eff6c1e6fab62a4a0c0ace62aa0087e6e
athar <athershaikh83@gmail.com>
2022-06-05 16:06:06 +0530

    changed by shaikh athar

---
+++ cal.js
@@ -0,0 +1,18 @@
+// import {add} from './add.js'
+// import {printResult} from './add.js'
+// import {q} from './add.js'
+// import {add1} from './add.js'
+import*as test from'./add.js'
+
+// let z=test.add(20,100)
+// console.log(z);
+// printResult();
+// let a=q;
+// console.log(z);
+// let b=add1(20,20);
+// console.log(b);
+
+console.log(test.add(2,3));
+test.printResult();
+console.log(test.q);
+console.log(test.add1(4,4));
